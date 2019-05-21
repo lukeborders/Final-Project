033191f8ebc597c1788e78cf0b614d021adcd1db
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2019-05-21 16:58:51 -0700

    Update Main.java
    Displays the player, and the game loop works. Need to work on event handlers as they do not work but there is no syntax error.

--- Main.java
+++ Main.java
@@ -2,14 +2,18 @@
 //import necessary packages before method usage.
 import javafx.application.Application;  
 import javafx.event.ActionEvent;  
-import javafx.event.EventHandler;  
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 import javafx.scene.Scene;  
 import javafx.scene.control.Button;  
 import javafx.stage.Stage;  
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.*;
 import javafx.scene.shape.*;
-import java.utils.Color
+import javafx.animation.AnimationTimer;
+import java.awt.Color;
+
 
 
 public class Main extends Application{ //extend application 
@@ -19,12 +23,11 @@ public class Main extends Application{ //extend application
         root.getChildren().add(startButton); //adds the button (child) node to the root StackPane (parent). If for some reason you dont understand the hierarchy concept and the Parent-->child read up on a gosh darn JavaFX tutorial  
         Scene mainScene = new Scene(root,600,400); // creates a scene which is basically a window right? I have no idea bud. Well it has a 600x400 WidthxLength so not sure if window or what
         stage.setScene(mainScene); //sets the scene for the stage i guess.
-        stage.setTitle("Dumb Temple run knockoff"); //just makes a title for the scene
+        stage.setTitle("Square Game"); //just makes a title for the scene
         stage.show(); // its literally in the method name.
-        Group sprites = new Group();
         int initX = 200;
         int initY = 150;
-      
+        final Player player = new Player(initX,initY,20,20,0); //y velocity is placeholder
         //attempt on a game loop:
         //using tutorial on nano time:
         final long startTime = System.nanoTime(); //variable for storing the start of the game  I think
@@ -32,44 +35,36 @@ public class Main extends Application{ //extend application
             public void handle(long currentTime) {
                 double elapsed = (currentTime - startTime)/1000000000.0; //variable for storing the calculated elapsed time
                 
-                
                 startButton.setOnAction(new EventHandler<ActionEvent>(){
                     public void handle(ActionEvent arg0) {
                         StackPane gamePane = new StackPane();
+                        gamePane.getChildren().add(player.getPlayer());
                         Scene game = new Scene(gamePane,600,400);
                         stage.setScene(game);
                         stage.show();
-                        Canvas canvas = new Canvas(600,400);
-                        Player player = new Player(initX,initY,20,20,0); //y velocity is placeholder
                         
-                       /* Rectangle r = new Rectangle()
-                        r.setWidth(20);
-                        r.setHeight(20);
-                        r.setFill(Color.blue);   
-                        sprites.getChildren().add(r); */
-                        
-                        //sprites.getChildren().add(player);
-                }
-                    EventHandler<KeyEvent> keyEventHandler = new EventHandler<KeyEvent>() {
+                       
+                    }
+                    EventHandler RightKeyEventHandler = new EventHandler<KeyEvent>() {
                         public void handle(KeyEvent event) {
-                            if(event.getCode() == keyCode.RIGHT) {
-                                //player.xPosition += 100;  assuming each lane is about 100 or so pixels wide
-                                //player.update(); will create a update method that will update the player
+                            if(event.getCode() == KeyCode.RIGHT) {
+                                System.out.println("Right Key");
                             }
                         }
-                    }
-                    EventHandler<KeyEvent> keyEventHandler = new EventHandler<KeyEvent>() {
-                        public void handle(keyEvent event) {
-                            if(event.getCode() == keyCode.LEFT) {
-                                //player.getXPosition() += -100
-                                //player.update();
+                    };
+                    EventHandler LeftKeyEventHandler = new EventHandler<KeyEvent>() {
+                        public void handle(KeyEvent event) {
+                            if(event.getCode() == KeyCode.LEFT) {
+                                System.out.println("Left key");
                             }
                         }
-                    }
+                    };
                 });
-            }.start();
-        }
-    public static void Main(String[] args) { //ALWAYS NEED A MAIN METHOD ;)
+                player.update(elapsed); 
+            }
+        }.start();
+    }
+    public static void main(String[] args) { //ALWAYS NEED A MAIN METHOD ;)
         launch(args); //method that launches the program when main method is called on startup.
     }
 
