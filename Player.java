0d28ab3d30ab3ad845c7955efb6717336f965622
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2019-05-21 14:55:36 -0700

    Update Player.java
    Constructor initializes a Rectangle that will represent the player. Update method that updates the y position of the player. Will work on updating x position according to key presses.

--- Player.java
+++ Player.java
@@ -1,41 +1,19 @@
+import javafx.scene.shape.*;
 public class Player {
-  private int xpos;
-	private int ypos;
-	private int velocity;
-	private int height;
-	private int width;
-
-  public Player(int x, int y, int v) 
-	{
-    xpos = X;
-		ypos = Y;
-		velocity = v;
-		new Rectangle(x,y);
-  }
-
-    
-  public void setX(int x) 
-	{
-    xpos = x;
-  }
-	public void setY(int y) 
-	{
-    ypos = y;
-  }
-	public void setSpeed(int v) 
-	{
-    velocity = v;
-   }
-  public int getXPos() 
-	{
-    return xpos;
-  }
-	public int getYPos() 
-	{
-    return ypos;
-  }
-	public int getVelocity() 
-	{
-    return velocity;
-  }
+	private int xPos;
+	private int yPos;
+	private Rectangle playerSprite;
+	private double yVelocity;
+	public Player(int x, int y, int height, int width, double yv) {
+		xPos = x;
+		yPos = y;
+		yVelocity = yv;
+		playerSprite = new Rectangle(height,width,x,y);	
+	}
+	public Rectangle update(double etime) { //needs to update position according to the time elapsed;
+		yPos += yVelocity * etime;	
+	}
+	public void render() {
+		playerSprite = new Rectangle(height,width,xPos,yPos);		
+	}
 }
