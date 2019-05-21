e6ab202ac608876ea24379b94c5ff2f2ee3da155
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2019-05-21 15:28:41 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,6 +2,8 @@ import javafx.scene.shape.*;
 public class Player {
 	private int xPos;
 	private int yPos;
+	private boolean lorf; //true if left key pressed, false if right key pressed
+	//private int laneNum; // 0 for lane one, 1 for lane two , and 2 for lane three
 	private Rectangle playerSprite;
 	private double yVelocity;
 	public Player(int x, int y, int height, int width, double yv) {
@@ -11,7 +13,7 @@ public class Player {
 		playerSprite = new Rectangle(height,width,x,y);	
 	}
 	public Rectangle update(double etime) { //needs to update position according to the time elapsed;
-		yPos += yVelocity * etime;	
+		yPos += yVelocity * etime;
 	}
 	public void render() {
 		playerSprite = new Rectangle(height,width,xPos,yPos);		
