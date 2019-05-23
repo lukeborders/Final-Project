9e6ea663154b67fb726e6a6b68a2d604d9fa35f5
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2019-05-22 18:11:48 -0700

    Update Main.java
    added boundaries

--- Main.java
+++ Main.java
@@ -12,38 +12,49 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.*;
 import javafx.scene.shape.*;
 import javafx.animation.AnimationTimer;
-import java.awt.Color;
 import javafx.scene.Node.*;
 import javafx.application.*;
 import javafx.application.Platform;
 import javafx.application.Platform.*;
-
-
-
-
-
+import javafx.scene.text.*;
+import javafx.scene.paint.Color;
 public class Main extends Application{ //extend application 
     public void start(Stage stage) { // stage is basically a window; also start method is run when the program is run so everything that needs to be done when the program first starts is to be put here
-        Button startButton = new Button("Start"); // example of a node or element is a button
+        Button startButton = new Button("START"); // example of a node or element is a button
+        Text mainText = new Text("Welcome to the Square Game");
+        Text subText = new Text("Please Press START to continue!");
+        subText.setFill(Color.LAWNGREEN);
+        subText.setTranslateY(-85.0);
+        mainText.setFill(Color.BLUE);
+        mainText.setTranslateY(-100.0);
         StackPane root = new StackPane(); // StackPanes basically are like a base node to put all other nodes on. Also nodes have like a hierarchy. Will explain in class.
+        root.getChildren().add(mainText);
+        root.getChildren().add(subText);
         root.getChildren().add(startButton); //adds the button (child) node to the root StackPane (parent). If for some reason you dont understand the hierarchy concept and the Parent-->child read up on a gosh darn JavaFX tutorial  
         Scene mainScene = new Scene(root,600,400); // creates a scene which is basically a window right? I have no idea bud. Well it has a 600x400 WidthxLength so not sure if window or what
         stage.setScene(mainScene); //sets the scene for the stage i guess.
         stage.setTitle("Square Game"); //just makes a title for the scene
         stage.show(); // its literally in the method name.
-
-//____________________________________________________________________
         int initX = 0;
         int initY = 0;
         StackPane gamePane = new StackPane();
         Scene game = new Scene(gamePane,600,400);
-        final Player player = new Player(initX,initY,20,20,0); //y velocity is placeholder
+        final Player player = new Player(initX,initY,20,20,0.0); //y velocity is placeholder
         Rectangle p = player.getPlayer();
-        //attempt on a game loop:
-        //using tutorial on nano time:
+        Line line1 = new Line(-100,0,-100,500);
+        Line line2 = new Line(100,0,100,500);
+        //Barrel b = new Barrel(((int)Math.random()*150),((int)Math.random()*500),0,45);
+
+
         startButton.setOnAction(new EventHandler<ActionEvent>(){ //start button handler
             public void handle(ActionEvent arg0) {
                 gamePane.getChildren().add(p);
+                System.out.println(p);
+                gamePane.getChildren().add(line1);
+                gamePane.getChildren().add(line2);
+                //gamePane.getChildren().add(b.getBarrel());
+                line1.setTranslateX(-200.0);
+                line2.setTranslateX(200.0);
                 stage.setScene(game);
                 stage.show();
             }
@@ -52,12 +63,13 @@ public class Main extends Application{ //extend application
         EventHandler<KeyEvent> RightKeyEventHandler = new EventHandler<KeyEvent>() { //detects right key press for future movement
             public void handle(KeyEvent event) {
                 if(event.getCode() == KeyCode.RIGHT) {
-                    System.out.println("Right Key");
-                    player.setCurrentPos(initX+50);
-                    player.setXPos(player.getCurrentPos());
-                    player.render();
-                    event.consume();
-
+                    if(player.getCurrentXPos() < 150) {
+                        System.out.println("Right Key");
+                        player.setCurrentXPos(player.getXPos()+50);
+                        player.setXPos(player.getCurrentXPos());
+                        player.render();
+                        event.consume();
+                    }
                 }  
             }
         };
@@ -66,14 +78,18 @@ public class Main extends Application{ //extend application
             public void handle(KeyEvent event) {
                 if(event.getCode() == KeyCode.LEFT) {
                     System.out.println("Left key");
-                    player.setCurrentPos(initX-50);
-                    player.setXPos(player.getCurrentPos());
-                    player.render();
+                    if(player.getCurrentXPos() > -150) {
+                        player.setCurrentXPos(player.getXPos()-50);
+                        player.setXPos(player.getCurrentXPos());
+                        System.out.println("Current Position: " + player.getCurrentXPos());
+                        player.render();
+                    }
+                    
+                    
                     event.consume();
                 }
             };
         };
-
         EventHandler<KeyEvent> escapeHandler = new EventHandler<KeyEvent>() { //detects escape key press for closing window
             public void handle(KeyEvent event) {
                 if(event.getCode()==KeyCode.ESCAPE) {
@@ -96,9 +112,8 @@ public class Main extends Application{ //extend application
             public void handle(long currentTime) {
                 double elapsed = (currentTime - startTime)/1000000000.0; //variable for storing the calculated elapsed time
                 if(Platform.isFxApplicationThread()==true) {
-                    if(player.getCurrentPosition() < 200) {
-                        p.setTranslateX(player.getXPos());
-                    }
+                    p.setTranslateX(player.getXPos());
+                    
                 
                 }
 
@@ -116,4 +131,10 @@ public class Main extends Application{ //extend application
     public static void main(String[] args) { //ALWAYS NEED A MAIN METHOD ;)
         launch(args); //method that launches the program when main method is called on startup.
     }
+   /*public static Obstacle getRandomObstacle() {
+        Obstacle b = new Barrel(Math.random()*150,Math.random()*500,45);
+        Obstacle[] obstacle = {b};
+        return obstacle[Math.random()*obstacle.length];
+   }
+   */
 }
